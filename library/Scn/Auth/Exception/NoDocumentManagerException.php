2c51702d5562c739576277b755ebada46c2cfdda
Nicholas Calugar <njcalugar@gmail.com>
2011-03-11 10:37:25 -0800

    Can't get namespaces working properly

---
+++ library/Scn/Auth/Exception/NoDocumentManagerException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * SocalNick PHP Library
+ *
+ * LICENSE
+ *
+ * FREE AS IN BEER
+ *
+ * @category   Scn
+ * @package    Scn_Auth
+ * @subpackage Exception
+ * @copyright  Copyright (c) Nicholas Calugar (https://github.com/njcalugar/)
+ * @license    https://github.com/njcalugar/SocalNick-PHP-Library/blob/master/LICENSE
+ */
+
+namespace Scn\Auth\Exception;
+
+class NoDocumentManagerException extends \Exception
+{
+}
